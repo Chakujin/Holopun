58cb5bef540dc2d00375be481a006f7deb40cadf
Chaku <cristiangalindo13@gmail.com>
2022-10-12 21:00:07 +0200

    OVR

--- Holopun_Game/Assets/Scripts/Player/FixHeadRotation.cs
+++ Holopun_Game/Assets/Scripts/Player/FixHeadRotation.cs
@@ -4,22 +4,30 @@ using UnityEngine;
 
 public class FixHeadRotation : MonoBehaviour
 {
-    [SerializeField] private Transform currenTransform;
+    [SerializeField] private Transform target;
+    //[SerializeField] private Transform COG;
 
-    [SerializeField]private float fixRot;
+    [SerializeField] private float fixRot;
+    [SerializeField] private float COGfix;
+    public Vector3 startPos;
+
 
     // Update is called once per frame
     void Update()
     {
-        fixRot = currenTransform.localEulerAngles.y;
-        
+        transform.rotation = target.rotation;
+
+        /*
         if(fixRot > 60 && fixRot < 300)
         {
             Debug.Log("Positivo");
+            COG.localEulerAngles = new Vector3(0,0,fixRot);
         }
         if(fixRot > 300 && fixRot > 60)
         {
             Debug.Log("Negativo");
+            COG.localEulerAngles = new Vector3(0, 0, fixRot);
         }
+        */
     }
 }
