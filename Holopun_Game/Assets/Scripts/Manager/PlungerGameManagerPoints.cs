5a5db7c922d7c54aecc09a2c9cb5d1228c7cc0a4
Chaku <cristiangalindo13@gmail.com>
2022-10-18 17:24:39 +0200

    Start Scoreboard

--- Holopun_Game/Assets/Scripts/Manager/PlungerGameManagerPoints.cs
+++ Holopun_Game/Assets/Scripts/Manager/PlungerGameManagerPoints.cs
@@ -1,12 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlungerGameManagerPoints : MonoBehaviour
 {
+    [SerializeField] private Transform entryContainer;
+    [SerializeField] private Transform entryTemplate;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        entryTemplate.gameObject.SetActive(false);
+
+        float templateHeight = 0.35f; // space between templates
+        //Generate Templates
+        for (int i = 0; i < 10; i++) //Can change 10 by total num players
+        {
+            Transform entryTransform = Instantiate(entryTemplate, entryContainer); //Instance template
+            RectTransform entryRectTransform = entryTransform.GetComponent<RectTransform>();
+            entryRectTransform.anchoredPosition = new Vector2(0, -templateHeight * i); // Move template
+            entryTransform.gameObject.SetActive(true);
+        }
     }
 }
