476f7cd984d1e3b66d5c17735032d132c14aee3a
Chaku <cristiangalindo13@gmail.com>
2022-10-27 21:15:18 +0200

    Start Crosbow

--- Holopun_Game/Assets/Scripts/Character/FindInteractionManager.cs
+++ Holopun_Game/Assets/Scripts/Character/FindInteractionManager.cs
@@ -3,11 +3,23 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class FindInteractionManager : MonoBehaviour
 {
-    [SerializeField] private XRDirectInteractor myInteractor;
+    [SerializeField] private XRDirectInteractor myDirectInteractor;
+    [SerializeField] private XRRayInteractor myRayInteractor;
+
+    [SerializeField] private bool b_Ray = false;
+
     // Start is called before the first frame update
     void Awake()
     {
-        myInteractor.interactionManager = GameObject.FindGameObjectWithTag("InteractionManager")
+        if(b_Ray == false)
+        {
+            myDirectInteractor.interactionManager = GameObject.FindGameObjectWithTag("InteractionManager")
             .GetComponent<XRInteractionManager>();
+        }
+        else
+        {
+            myRayInteractor.interactionManager = GameObject.FindGameObjectWithTag("InteractionManager")
+            .GetComponent<XRInteractionManager>();
+        }
     }
 }
