4bd6c9728aef7c914425fb716072e821cd56d703
Chaku <cristiangalindo13@gmail.com>
2022-11-01 22:16:05 +0100

    Arrow Pool

--- Holopun_Game/Assets/Scripts/Props/Crossbow/ArrowPoolObject.cs
+++ Holopun_Game/Assets/Scripts/Props/Crossbow/ArrowPoolObject.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class ArrowPoolObject : MonoBehaviour
 {
+    [SerializeField] private int i_amount;
+    [SerializeField] private GameObject ArrowPref;
+    private List<GameObject> arrowsList = new List<GameObject>();
     // Start is called before the first frame update
     void Start()
     {
-        
+        AddArrowToPool(i_amount);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void AddArrowToPool(int amount)
     {
-        
+        for (int i = 0; i < amount; i++)
+        {
+            GameObject arrow = Instantiate(ArrowPref);
+            arrow.SetActive(false);
+            arrowsList.Add(arrow);
+            arrow.transform.parent = transform;
+        }
+    }
+
+    public GameObject RquestArrow()
+    {
+        for (int i = 0; i<arrowsList.Count; i++)
+        {
+            if (arrowsList[i].activeSelf)
+            {
+                arrowsList[i].SetActive(true);
+                return arrowsList[i];
+            }
+        }
+
+        return null;
     }
 }
